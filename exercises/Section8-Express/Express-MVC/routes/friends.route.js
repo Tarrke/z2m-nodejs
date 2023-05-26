8b0ef5d3a18ad3cc462899733d7581effb3e0a31
Tarrke <tarrke@gmail.com>
2023-05-26 13:54:15 +0200

    add specific response for bad requests

--- exercises/Section8-Express/Express-MVC/routes/friends.route.js
+++ exercises/Section8-Express/Express-MVC/routes/friends.route.js
@@ -11,5 +11,8 @@ friendsRouter.use((req, res, next) => {
 friendsRouter.get ('/:friendId', friendsController.getFriend);
 friendsRouter.get ('/',          friendsController.getFriends);
 friendsRouter.post('/',          friendsController.addFriend);
+friendsRouter.post('/:friendId', (req, res) => {
+    res.status(404).json({error: 404, message: 'You can\'t use post on a specific item.'});
+})
 
 module.exports = friendsRouter;
