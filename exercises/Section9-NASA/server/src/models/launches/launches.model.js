6c1d093849332cceade8542ec776832de4d1e4fa
Tarrke <tarrke@gmail.com>
2023-06-13 07:44:27 +0200

    end of /launches endpoint

--- exercises/Section9-NASA/server/src/models/launches/launches.model.js
+++ exercises/Section9-NASA/server/src/models/launches/launches.model.js
@@ -23,11 +23,27 @@ const launch_2 = {
     success: true,
 }
 
+const launch_3 = {
+    flightNumber: 101,
+    launchDate: new Date('December 27, 2022'),
+    mission: "Kepler exploration X",
+    rocket: "Explorer IS1",
+    target: "Kepler-442 b",
+    customers: ["NASA","TKC Corp"],
+    upcoming: false,
+    success: true,
+}
+
+launches_by_id.set(launch_3.flightNumber, launch_3);
+launches_by_name.set(launch_3.mission_name, launch_3);
+
 launches_by_id.set(launch_2.flightNumber, launch_2);
 launches_by_name.set(launch_2.mission_name, launch_2);
 
 launches_by_id.set(launch.flightNumber, launch);
 launches_by_name.set(launch.mission_name, launch);
+
+
 
 module.exports = {
     launches_by_id,
